6f4ef80b92992c4d8774d8a1f049d0f501cab216
Diego Magela <diegomagela@hotmail.com>
2023-12-05 09:28:31 +0000

    add namespace for better readability

--- include/VectorUtil.hpp
+++ include/VectorUtil.hpp
@@ -7,55 +7,78 @@
 
 #include <Eigen/Dense>
 
-template <class T>
-void write_vector(std::vector<T> vector, std::string filename)
+namespace vct
 {
-    std::ofstream file(filename);
-
-    if(file.is_open())
+    template <class T>
+    void write_vector(std::vector<T> vector, std::string filename)
     {
+        std::ofstream file(filename);
 
+        if (file.is_open())
+        {
+
+            std::cout << '{';
+
+            for (std::size_t i = 0; i < vector.size() - 1; ++i)
+                std::cout << vector[i] << ", ";
+
+            std::cout << vector.back();
+
+            std::cout << '}';
+        }
     }
 
-    std::cout << '{';
+    template <class T>
+    void print_vector(std::vector<T> vector)
+    {
+        std::cout << '{';
 
-    for (std::size_t i = 0; i < vector.size() - 1; ++i)
-        std::cout << vector[i] << ", ";
+        for (std::size_t i = 0; i < vector.size() - 1; ++i)
+            std::cout << vector[i] << ", ";
 
-    std::cout << vector.back();
+        std::cout << vector.back();
 
-    std::cout << '}';
-}
+        std::cout << '}';
 
-template <class T>
-void print_vector(std::vector<T> vector)
-{
-    std::cout << '{';
+        std::cout << std::endl;
+    }
 
-    for (std::size_t i = 0; i < vector.size() - 1; ++i)
-        std::cout << vector[i] << ", ";
+    // Append vector B in the end of vector A
+    template <class T>
+    inline void append_vector(std::vector<T> &a, const std::vector<T> &b)
+    {
+        a.insert(a.end(), b.begin(), b.end());
+    }
 
-    std::cout << vector.back();
+    // Check if vector a contains element x
+    template <class T>
+    inline bool vector_has_element(const std::vector<T> &a, const T x)
+    {
+        if (std::find(a.begin(), a.end(), x) != a.end())
+            return true;
 
-    std::cout << '}';
-}
+        else
+            return false;
+    }
 
-template <class T>
-inline void append_vector(std::vector<T> a, const std::vector<T> b)
-{
-    a.insert(std::end(a), std::begin(b), std::end(b));
-}
+    //
+    template <class T>
+    std::vector<T> extract_elements(const std::vector<T> &vector,
+                                    const std::size_t first,
+                                    const std::size_t last)
+    {
+        std::size_t size = last - first + 1; // number of elements
+        std::vector<T> new_vector;
+        new_vector.reserve(size);
 
-// Check if vector a contains element x
-template <class T>
-inline bool vector_has_element(const std::vector<T> a, const T x)
-{
-    if (std::find(a.begin(), a.end(), x) != a.end())
-        return true;
+        for (std::size_t i = first; i <= last; ++i)
+            new_vector.push_back(vector.at(i));
 
-    else
-        return false;
-}
+        return new_vector;
+    }
+} // namespace vecutl
+
+// FUNCTIONS TO CONSULT
 
 // Write Eigen matrix to CSV format file
 
