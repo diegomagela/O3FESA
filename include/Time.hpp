ed94b32fe14984d92aaedf67661d69e188b5c46a
Diego Magela <diegomagela@hotmail.com>
2023-12-14 19:27:35 +0000

    Header file for time measuring

---
+++ include/Time.hpp
@@ -0,0 +1,10 @@
+#ifndef TIME_HPP
+#define TIME_HPP
+
+#include <chrono>
+
+// const auto start{std::chrono::high_resolution_clock::now()};
+// const auto end{std::chrono::high_resolution_clock::now()};
+// const std::chrono::duration<double> elapsed_seconds{end - start};
+
+#endif // TIME_HPP
