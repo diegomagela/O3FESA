55881355afa921f648c464ecae0af04a24d55949
Diego Magela <diegomagela@hotmail.com>
2023-12-14 19:20:32 +0000

    Add new functions to get dof displacements

--- include/Dofs.hpp
+++ include/Dofs.hpp
@@ -28,6 +28,7 @@ public:
                                               accelerations_(accelerations) {}
 
     // The rule of five
+
     Dofs() = default;
     Dofs(Dofs const &) = default;
     Dofs &operator=(Dofs const &) = default;
@@ -39,17 +40,22 @@ public:
     // Return dofs displacements
     inline std::vector<double>
     get_displacements() const { return displacements_; }
-
     // Return dofs velocities
     inline std::vector<double>
     get_velocities() const { return velocities_; }
-
     // Return dofs accelerations
     inline std::vector<double>
     get_accelerations() const { return accelerations_; }
-
+    // Return u displacement
+    inline double get_u_displacement() const { return displacements_.at(0); }
+    // Return v displacement
+    inline double get_v_displacement() const { return displacements_.at(1); }
     // Return w displacement
     inline double get_w_displacement() const { return displacements_.at(2); }
+    // Return v displacement
+    inline double get_phi_x_displacement() const { return displacements_.at(3); }
+    // Return w displacement
+    inline double get_phi_y_displacement() const { return displacements_.at(4); }
 
 private:
     std::vector<double> displacements_{0, 0, 0, 0, 0};
